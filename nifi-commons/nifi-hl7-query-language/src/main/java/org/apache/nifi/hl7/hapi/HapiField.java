d9b94bf42a81991ca0270fa0b0e6c1cfc8fb01f8
vivekkoya <67130044+vivekkoya@users.noreply.github.com>
2025-05-10 20:18:22 -0700

    Update HapiField.java by making typecasting safer
    https://openjdk.org/jeps/394

--- nifi-commons/nifi-hl7-query-language/src/main/java/org/apache/nifi/hl7/hapi/HapiField.java
+++ nifi-commons/nifi-hl7-query-language/src/main/java/org/apache/nifi/hl7/hapi/HapiField.java
@@ -50,8 +50,8 @@ public class HapiField implements HL7Field, HL7Component {
         final ExtraComponents extra = type.getExtraComponents();
         if (extra != null && extra.numComponents() > 0) {
             final String singleFieldValue;
-            if (type instanceof Primitive) {
-                singleFieldValue = ((Primitive) type).getValue();
+            if (type instanceof Primitive t) {
+                singleFieldValue = t.getValue();
             } else {
                 singleFieldValue = this.value;
             }
